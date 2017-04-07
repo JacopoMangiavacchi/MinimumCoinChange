61eec9c0471bd94286f7d8e21e3b6aeb12367aff
Jacopo Mangiavacchi <Jacopo.Mangiavacchi@uk.ibm.com>
2017-04-07 11:26:08 +0200

    implemented local cache in dynamicChange func using nested functions

--- Sources/MinimumCoinChange.swift
+++ Sources/MinimumCoinChange.swift
@@ -13,11 +13,9 @@ public enum MinimumCoinChangeError: Error {
 
 public struct MinimumCoinChange {
     private let sortedCoinSet: [Int]
-    private var cache: [Int : [Int]]
     
     public init(coinSet: [Int]) {
         self.sortedCoinSet = coinSet.sorted(by: { $0 > $1} )
-        self.cache = [:]
     }
 
     //Greedy Algorithm
@@ -46,43 +44,55 @@ public struct MinimumCoinChange {
     }
 
     //Dynamic Programming Algorithm
-    public mutating func changeDynamic(_ value: Int, shouldThrow: Bool = true) throws -> [Int] {
+    public func changeDynamic(_ value: Int) throws -> [Int] {
         guard value > 0 else { return [] }
         
-        if let cached = cache[value] {
-            return cached
-        }
-        
-        var change:  [Int] = []
-        
-        var potentialChangeArray: [[Int]] = []
-        
-        for coin in sortedCoinSet {
-            if value - coin >= 0 {
-                var potentialChange: [Int] = []
-                potentialChange.append(coin)
-                let newPotentialValue = value - coin
+        var cache: [Int : [Int]] = [:]
 
-                if value  > 0 {
-                    potentialChange.append(contentsOf: try changeDynamic(newPotentialValue, shouldThrow: false))
-                }
+        func _changeDynamic(_ value: Int) -> [Int] {
+            guard value > 0 else { return [] }
 
-                //print("value: \(value) coin: \(coin) potentialChange: \(potentialChange)")
-                potentialChangeArray.append(potentialChange)
+            if let cached = cache[value] {
+                return cached
             }
+            
+            var change: [Int] = []
+            
+            var potentialChangeArray: [[Int]] = []
+            
+            for coin in sortedCoinSet {
+                if value - coin >= 0 {
+                    var potentialChange: [Int] = []
+                    potentialChange.append(coin)
+                    let newPotentialValue = value - coin
+
+                    if value  > 0 {
+                        potentialChange.append(contentsOf: _changeDynamic(newPotentialValue))
+                    }
+
+                    //print("value: \(value) coin: \(coin) potentialChange: \(potentialChange)")
+                    potentialChangeArray.append(potentialChange)
+                }
+            }
+            
+            if potentialChangeArray.count > 0 {
+                let sortedPotentialChangeArray = potentialChangeArray.sorted(by: { $0.count < $1.count })
+                change = sortedPotentialChangeArray[0]
+            }
+            
+            if change.reduce(0, +) == value { 
+                cache[value] = change
+            } 
+
+            return change
         }
-        
-        if potentialChangeArray.count > 0 {
-            let sortedPotentialChangeArray = potentialChangeArray.sorted(by: { $0.count < $1.count })
-            change = sortedPotentialChangeArray[0]
-        }
-        
-        if shouldThrow && change.reduce(0, +) != value { 
+
+        let change: [Int] = _changeDynamic(value)
+
+        if change.reduce(0, +) != value { 
             throw MinimumCoinChangeError.noRestPossibleForTheGivenValue 
         } 
 
-        cache[value] = change
         return change
     }
 }
-
